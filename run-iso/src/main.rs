a68758bcbce5de876aa384ce44ddf7b367a3f546
Ray Redondo <raydredondo@gmail.com>
2024-05-14 23:18:34 -0500

    feat(tools): run-iso works, updated limine.cfg

--- run-iso/src/main.rs
+++ run-iso/src/main.rs
@@ -1,3 +1,30 @@
+#![feature(exit_status_error)]
+
+use std::process::Command;
+
 fn main() {
+    Command::new("cargo")
+        .arg("iso")
+        .status()
+        .expect("iso build failed")
+        .exit_ok()
+        .expect("iso build failed");
     
+    Command::new("qemu-system-x86_64")
+        .args([
+            "-M",
+            "q35",
+            "-m",
+            "2G",
+            "-bios",
+            "ovmf/OVMF.fd",
+            "-cdrom",
+            "lilium.iso",
+            "-boot",
+            "d",
+        ])
+        .status()
+        .expect("error running iso")
+        .exit_ok()
+        .expect("error running iso");
 }
