bb0151a460c4b542cb51e2306918d38482e7adda
Ray Redondo <raydredondo@gmail.com>
2024-05-14 21:16:08 -0500

    feat(loader): initialize dynloader with correct base address

--- loader/src/lib.rs
+++ loader/src/lib.rs
@@ -5,14 +5,22 @@ mod elf;
 
 use core::panic::PanicInfo;
 
-use limine::BaseRevision;
+use limine::{request::KernelAddressRequest, BaseRevision};
 
 #[used]
 static BASE_REVISION: BaseRevision = BaseRevision::new();
 
+#[used]
+static KERNEL_ADDRESS: KernelAddressRequest = KernelAddressRequest::new();
+
 #[no_mangle]
 unsafe extern "C" fn _start() -> ! {
-    dynloader::init_dyn_loader(0);
+    dynloader::init_dyn_loader(
+        KERNEL_ADDRESS
+            .get_response()
+            .map(|x| x.virtual_base() as usize)
+            .unwrap_or(0),
+    );
     assert!(BASE_REVISION.is_supported());
     loop {}
 }
